33f3d26c0c47443ba93e27f9afd37d139ff388a2
Kavita Kar <Kavita.Kar@group1.com>
2025-07-09 16:56:28 -0400

    UI

--- app.py
+++ app.py
@@ -3,4 +3,4 @@ import streamlit as st
 st.set_page_config(page_title="Options Trainer")
 
 st.title("Options Practice Suite")
-st.write("Use the button below to launch the interactive trader.")
+st.write("Use the sidebar to launch the interactive trader.")
